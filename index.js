ddaa76552e300f02ddfa6195ea7ac19b6caae5c1
vemir8 <vemir@aobyte.com>
2021-03-24 11:13:59 +0400

    Switch to Typescript

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-import 'react-native-gesture-handler';
-import * as React from "react";
 import { registerRootComponent } from 'expo';
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import store from './app/store';
-import App from './app/App';
-import i18n from './app/i18n';
 
-const Index = () => (
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </Provider>
-);
+import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Index);
+registerRootComponent(App);
