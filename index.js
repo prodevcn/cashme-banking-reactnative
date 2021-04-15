4befa02de8f74ac7aaf547d24b13322d4c8932d0
Hayk <hayk.pilos@gmail.com>
2021-04-16 00:34:21 +0400

    Basic theme setup

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ import { registerRootComponent } from "expo";
 import { Provider } from "react-redux";
 import { I18nextProvider } from "react-i18next";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Root } from "native-base";
+import { Root, StyleProvider } from "native-base";
+import getTheme from './native-base-theme/components';
 import store from "./app/store";
 import i18n from "./app/i18n";
 import App from "./app/App";
 
 const RootComponent = () => (
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <SafeAreaProvider>
-        <Root>
-          <App />
-        </Root>
-      </SafeAreaProvider>
-    </I18nextProvider>
-  </Provider>
+  <StyleProvider  style={getTheme()}>
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <SafeAreaProvider>
+          <Root>
+            <App />
+          </Root>
+        </SafeAreaProvider>
+      </I18nextProvider>
+    </Provider>
+  </StyleProvider>
 );
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
