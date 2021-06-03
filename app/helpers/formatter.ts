f524df0c8a96aa47bdd9606a1cdb45835dee48ae
Hayk <hayk.pilos@gmail.com>
2021-06-03 15:22:39 +0400

    Remove unused intl

--- app/helpers/formatter.ts
+++ app/helpers/formatter.ts
@@ -1,8 +1,4 @@
 export const numberToDecimalString = (number: number, precision = 2) => {
-  const formatter = new Intl.NumberFormat("en", {
-    style: "decimal",
-    minimumFractionDigits: precision,
-  });
   return isNaN(number)
     ? ""
     : number.toFixed(precision).replace(/\d(?=(\d{3})+\.)/g, "$&,");
