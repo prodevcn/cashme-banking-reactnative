6d2c52bcc1bdc8d3fa1400db4874690f6a956648
Hayk <hayk.pilos@gmail.com>
2021-06-07 01:54:29 +0400

    Introduce build versioning

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,16 @@ else
   notes=$2
 fi
 
+# Increment the build version and tag
+npm version patch
+git push
+tag=$(git tag --points-at HEAD)
+git push origin "$tag"
+
 # Build apk
 cd android
 ./gradlew assembleRelease
 cd ..
 
-# # Deploy to firebase
+# Deploy to firebase
 firebase appdistribution:distribute ./android/app/build/outputs/apk/debug/app-debug.apk --app 1:499319607140:android:4af9e25d9e26fc8e58b471  --release-notes $notes --testers-file "./testers.txt" --token $token
